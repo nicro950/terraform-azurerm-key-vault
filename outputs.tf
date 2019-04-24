cf314dbdc596ee898382ba87f52c1c953e94df03
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2019-04-24 20:41:14 +0200

    feat: add `id` and `uri` outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "id" {
+  value       = azurerm_key_vault.main.id
+  description = "The ID of the Key Vault."
+}
+
+output "uri" {
+  value       = azurerm_key_vault.main.id
+  description = "The URI of the Key Vault."
+}
